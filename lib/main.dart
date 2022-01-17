9aa629ae0b0912e99ef720716a742e44486efb18
faisaljavid <csandhack1@gmail.com>
2022-01-17 15:37:02 +0500

    Hi Flutter A

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ import 'package:flutter/material.dart';
 
 // main() method is the entry point of our application, execution start from main().
 void main() {
-  // runApp() method is need a Widget as a parameter.
-  // We are passing TextWidget as a parameter which will be shown in the center of screen.
-  // As we are using Center()
-  // Great.
+
   runApp(const Center(
       child: Text("Hi Flutter", textDirection: TextDirection.ltr)));
 }
